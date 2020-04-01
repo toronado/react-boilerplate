b441cfdbcde1de9b9cbecb450d06ccd8cfdccfcf
toronado <redward.codes@gmail.com>
2020-04-01 17:51:11 +0100

    Used State

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import Button from '@material-ui/core/Button'
 
 function Main() {
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(prevState => {
+            return prevState + 1
+        })
+    }
+
     return (
         <section>
             <img src={logo} className="logo" alt="logo" />
-            <Button variant="contained" color="primary">
-                Hello World
+            <Button variant="contained" color="primary" onClick={increment}>
+                {count ? count : 'Hello World!'}
             </Button>
         </section>
     )
